f3827f67bc04b6f8858eddd3d8313f8f2d51a2e1
jyothika118 <jyothikasaladi9@gmail.com>
2025-11-28 02:27:52 +0530

    first commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1202",  // <=== BACKEND URL
+  baseURL: "http://localhost:3322",  // <=== BACKEND URL
   headers: { "Content-Type": "application/json" }
 });
 
